e126d49c75ec4d278ac4d3e6adaa388a916f3dc4
kunst1080 <kontrapunk1080@gmail.com>
2023-05-02 11:16:18 +0900

    SafeCommentが有効なとき、感情の強いコメントは登録させない

--- src/component/EmotionComponent.tsx
+++ src/component/EmotionComponent.tsx
@@ -25,15 +25,11 @@ const loadEmotion = async (): Promise<Emotion> => {
     if (!inputText) {
         return INITIAL_EMOTION;
     }
-    const prompt = `あなたは、世界でも有数の精神分析家です。
-文章から、著者の心理状態を分析することに長けています。
-次の文章をもとに心理分析してください。
-joy, anger, sadness, pleasureの4つの感情の値をそれぞれ0〜9の値で返答してください。
-返答の出力形式は、joy, anger, sadness, pleasureの順にカンマ区切りで出力してください。
-
-出力の例: 1,5,7,2
-
-分析したい文章: ${inputText}
+    const prompt = `課題管理システムのコメント欄から、書き手の感情の度合いを分析します。
+    以下の文章からjoy, anger, sadness, pleasureの4つの感情の値をそれぞれ0〜9の値で返答してください。
+    返答の出力形式は、joy, anger, sadness, pleasureの順にカンマ区切りで出力してください。
+    出力の例: 1,5,7,2
+    分析したい文章: ${inputText}
     `;
     const response = await loadData(
         calculateMD5(inputText),
@@ -51,11 +47,45 @@ joy, anger, sadness, pleasureの4つの感情の値をそれぞれ0〜9の値で
     };
 };
 
+const updateSubmitButton = (e: Emotion) => {
+    const button = document.querySelector(
+        "#switchStatusAddCommentForm button[type=submit]"
+    ) as HTMLInputElement;
+    if (!button) {
+        return;
+    }
+    if (e.anger > 5 || e.sadness > 5) {
+        button.textContent = "怒り・悲しみを抑えてコメントする";
+        button.disabled = true;
+    } else if (e.joy < 2 && e.pleasure < 2) {
+        button.textContent = "喜び・楽しみを増やしてコメントする";
+        button.disabled = true;
+    } else {
+        button.textContent = "登録";
+        button.disabled = false;
+    }
+};
+
+const getClassName = (emotion: Emotion): string => {
+    if (emotion == INITIAL_EMOTION) {
+        return "emotion-safe";
+    }
+    if (emotion.anger > 5 || emotion.sadness > 5) {
+        return "emotion-danger";
+    }
+    if (emotion.joy < 5 || emotion.pleasure < 5) {
+        return "emotion-warning";
+    }
+    return "emotion-safe";
+};
 export const EmotionComponent = () => {
     const [emotion, setEmotion] = React.useState<Emotion>(INITIAL_EMOTION);
     React.useEffect(() => {
         const update = () => {
-            loadEmotion().then((e) => setEmotion(e));
+            loadEmotion().then((e) => {
+                updateSubmitButton(e);
+                setEmotion(e);
+            });
         };
         const timer = setInterval(update, REQUEST_INTERVAL);
         // useEffectのクリーンアップ関数でタイマーをクリア
@@ -63,10 +93,12 @@ export const EmotionComponent = () => {
     }, []);
     return (
         <div>
-            <div>喜:{emotion.joy}</div>
-            <div>怒:{emotion.anger}</div>
-            <div>哀:{emotion.sadness}</div>
-            <div>楽:{emotion.pleasure}</div>
+            <div className={`emotion-area ${getClassName(emotion)}`}>
+                <div>喜:{emotion.joy}</div>
+                <div>怒:{emotion.anger}</div>
+                <div>哀:{emotion.sadness}</div>
+                <div>楽:{emotion.pleasure}</div>
+            </div>
         </div>
     );
 };
